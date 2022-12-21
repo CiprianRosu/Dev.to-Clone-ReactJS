81246839820009baa9a691686c6f751ab2bc692f
Ciprian Andrei Rosu <cip.rosu@yahoo.com>
2022-12-21 22:03:59 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import data from "./data.json";
+import Jobs from "./components/Jobs"
+import './scss/app.scss'
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+    <div className="header"></div>
+    <Jobs data= {data} />
     </div>
+    
   );
 }
 
---
+++ src/components/Job.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Job = (props) => {
+
+
+  return ( <div className="job-container">{props.data.location}</div>
+  
+  )
+  
+}
+
+export default Job;
